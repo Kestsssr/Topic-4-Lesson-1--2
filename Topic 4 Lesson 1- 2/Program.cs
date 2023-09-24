be1fdabb0c50e3192b336906e21ca15464ad88f3
Константин Головин <Kestsssr@gmail.com>
2023-09-24 19:14:36 +0200

    1.2

--- Topic 4 Lesson 1- 2/Program.cs
+++ Topic 4 Lesson 1- 2/Program.cs
@@ -3,9 +3,9 @@ int piterburg = 5598486;
 int moskva = 13015126;
 Console.WriteLine($"В Москве на {moskva - piterburg} жителей больше чем в Питербурге");
 
-////2
-//Decimal perYear = 260548.56895m;//За год
-//Decimal perMonth = perYear / 12;//За месяц
-//Decimal ndfl = perMonth / 100 * 13;//НДФЛ от з\п за месяц
-//Console.WriteLine($"Работник за месяц заработал {perMonth}");
-//Console.WriteLine($" работник за месяц раработал с вычетом НДФЛ {perMonth - ndfl}");
+//2
+Decimal perYear = 260548.56895m;//За год
+Decimal perMonth = perYear / 12;//За месяц
+Decimal ndfl = perMonth / 100 * 13;//НДФЛ от з\п за месяц
+Console.WriteLine($"Работник за месяц заработал {perMonth}");
+Console.WriteLine($" работник за месяц раработал с вычетом НДФЛ {perMonth - ndfl}");
