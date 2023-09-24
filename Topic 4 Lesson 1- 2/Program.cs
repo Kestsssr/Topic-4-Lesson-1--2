10729a802751eefca2e6a8e20d4ca8b65451e346
Константин Головин <Kestsssr@gmail.com>
2023-09-24 19:05:23 +0200

    Добавьте файлы проекта.

---
+++ Topic 4 Lesson 1- 2/Program.cs
@@ -0,0 +1,5 @@
+﻿Decimal perYear = 260548.56895m;//За год
+Decimal perMonth = perYear / 12;//За месяц
+Decimal ndfl = perMonth / 100 * 13;//НДФЛ от з\п за месяц
+Console.WriteLine($"Работник за месяц заработал {perMonth}");
+Console.WriteLine($" работник за месяц раработал с вычетом НДФЛ {perMonth - ndfl}");
